c753914c9792e5124198a1bafcd949456176b491
Back to the future <shrikant.swami@gmail.com>
2019-05-29 13:41:09 +0530

    Add files via upload

---
+++ src/test/java/runners/MyExecutorChrome2.java
@@ -0,0 +1,21 @@
+package runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/"},
+        glue     = {"stepDefinitions"},
+        format   = {
+                        "pretty",
+                        "html:target/site/cucumber-pretty2",
+                        "json:target/cucumber.json",
+        },
+        tags     = {"@trivial_chrome2"},
+        monochrome = true
+)
+public class MyExecutorChrome2 {
+
+}
